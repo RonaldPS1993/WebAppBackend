5be4fe87be32296d6e7a9e70265baecc5834cb07
JanP1020 <janperez1020@gmail.com>
2021-03-31 16:20:26 -0400

    Pushing updated bookDetails endpoints

--- api/bookDetails/bookDetails.router.js
+++ api/bookDetails/bookDetails.router.js
@@ -2,46 +2,28 @@
 const { request } = require('express');
 const express = require('express');
 const router = express.Router();
-const authordb = require('./authorInfo');
-const bookdb = require('./bookInfo');
+const booksdb = require('./bookInfo');
+const authorBooksdb = require('./authorBooks')
 
-//Router for /bookdetails/authorInfo which responds with all of the authors info if an id is not specified 
-router.get('/authorInfo', async (req,res, next)=>{
-    try{
-        let results = await authordb.allAuthors();
-        res.json(results);
-    }catch(e){
-        console.log(e);
-        res.sendStatus(500)
-    }
-});
 
-//Router for /bookdetails/:bookIsbn which responds with the author info that corressponds to the books isbn
-router.get('/authorInfo/:bookIsbn', async (req,res, next)=>{
-    try{
-        let results = await authordb.oneAuthor(req.params.bookIsbn);
-        res.json(results);
-    }catch(e){
-        console.log(e);
-        res.sendStatus(500)
-    }
-});
 
-//Router for /bookdetails/BookInfo which responds with the book title, book description,genre id,publisher id, and total rating for all books
-router.get('/bookInfo', async (req,res, next)=>{
-    try{
-        let results = await bookdb.allBooks();
-        res.json(results);
-    }catch(e){
-        console.log(e);
-        res.sendStatus(500)
-    }
-});
+
 
 //Router for /bookdetails/BookInfo/:bookIsbn which responds with the book title, book description,genre id,publisher id, and total rating for a given book
 router.get('/bookInfo/:bookIsbn', async (req,res, next)=>{
     try{
-        let results = await bookdb.oneBook(req.params.bookIsbn);
+        let results = await booksdb.oneBook(req.params.bookIsbn);
+        res.json(results);
+    }catch(e){
+        console.log(e);
+        res.sendStatus(500)
+    }
+});
+
+//Router for /bookdetails/authorBooks/:authorid which responds with the books done by a specific author based on the ID that is passed.
+router.get('/authorBooks/:authorId', async (req,res, next)=>{
+    try{
+        let results = await authorBooksdb.authorBooks(req.params.authorId);
         res.json(results);
     }catch(e){
         console.log(e);
