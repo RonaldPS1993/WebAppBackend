6c8ceb2e3200e4f70ebbde2b07b8be7f84d381e6
JanP1020 <janperez1020@gmail.com>
2021-03-31 16:22:23 -0400

    Pushing updated bookDetails Endpoints

--- api/bookDetails/bookInfo.js
+++ api/bookDetails/bookInfo.js
@@ -2,28 +2,34 @@ const pool = require("../../config/database");
 
 let booksdb = {};
 
-//Gets all of the book details (book title, book description,genre id,publisher id,total rating) from the database
-booksdb.allBooks = () =>{
-    return new Promise((resolve, reject)=>{
-        pool.query('SELECT book_title, book_description,genre_id,publisher_id,total_rating FROM books', (err,results)=>{
-            if(err){
-                return reject(err);
-            }
-            return resolve(results);
-        });
-    });
-};
 
-//Gets the an author's information according to the publisher id input
+//Gets the a books details based on the ISBN 
 booksdb.oneBook = (bookIsbn) => {
     return new Promise((resolve, reject)=>{
-        pool.query('SELECT book_title, book_description,genre_id,publisher_id,total_rating FROM books WHERE book_isbn = ?',[bookIsbn], (err,results)=>{
+        pool.query('SELECT book_title, book_description,genre_id,bio,total_rating FROM books WHERE book_isbn = ?',[bookIsbn], (err,bookInfo)=>{
             if(err){
                 return reject(err);
             }
-            return resolve(results[0]);
-        });
-    });
-};
+            pool.query('SELECT author_first_name, author_last_name, author_id FROM authors WHERE author_id = (select author_id FROM books where book_isbn = ?)',[bookIsbn], (err,authorInfo)=>{
+                if(err){
+                    return reject(err);
+                }
+                pool.query('SELECT genre_name FROM genre WHERE genre_id = (SELECT genre_id FROM books WHERE book_isbn =?)',[bookIsbn], (err,genreInfo)=>{
+                    if(err){
+                        return reject(err);
+                    }
+                    pool.query('SELECT * FROM publisher WHERE publisher_id = (SELECT publisher_id FROM books WHERE book_isbn =?)',[bookIsbn], (err,publishingInfo)=>{
+                        if(err){
+                            return reject(err);
+                        }
+                        return resolve({bookInfo,authorInfo,genreInfo,publishingInfo});
+                    })
+                })
+            })
+        })
+    })
+}    
+
 
 module.exports = booksdb;
+ 
--- app.js
+++ app.js
@@ -12,6 +12,19 @@ const userRouter = require("./api/users/user.router");
 app.use(express.json());
 app.use("/api/users", userRouter);
 
+//Handling CORS errors
+app.use((req,res,next)=>{
+  res.header('Access-Control-Allow-Origin','*');
+  res.header(
+  "Access-Control-Allow-Headers", 
+  "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if(req.method === 'OPTIONS'){
+    res.header('Access-Control-Allow-Methods','PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
 //bookdetails route
 const bookDetailsRoute = require("./api/bookDetails/bookDetails.router");
 app.use('/bookDetails', bookDetailsRoute);
