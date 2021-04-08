4abdeb6f95e0c82bc5ff7deb08b9920103dadc41
Daniel Riquelme <driqu001@fiu.edu>
2021-04-07 20:59:15 -0500

    updated wishlist backend

--- api/wishlist/wishlist.controller.js
+++ api/wishlist/wishlist.controller.js
@@ -1,15 +1,35 @@
 const { 
     createWish,
-    addBooksToWish
+    addBooksToWish,
+    getWish,
+    getWishNum,
+    changeWish,
+    deleteItem,
+    getShopping,
+    getCount
  } = require("./wishlist.service");
 const { decrypt } = require("../../auth/token_validation");
 
 module.exports = {
     createWishlist: (req,res) => {
         const body = req.body;      
-        const customer_id = decrypt(req).result.customer_id;
-        
+        //const customer_id = decrypt(req).result.customer_id; //FIXME front end tokens 
+        /*
         createWish(customer_id,body, (error,results)=> {
+            if(error){
+                console.log(error);
+                return res.status(500).json({
+                    success : 0,
+                    message : "Wishlist error"
+                });
+            }
+            return res.status(200).json({
+                success : 1,
+                data: results
+            });
+        }); */
+
+        createWish(body, (error,results)=> {
             if(error){
                 console.log(error);
                 return res.status(500).json({
@@ -38,6 +58,113 @@ module.exports = {
                 data: results
             });
         });
+    },
+    getWish:(req,res) => {
+        //const body = req.body;
+        //const customer_id = decrypt(req).result.customer_id;
+        const cart_id = req.params.cart_id;
 
+        getWish(cart_id, (error,results)=> {
+            if(error){
+                console.log(error);
+                return res.status(500).json({
+                    success : 0,
+                    message : "Wishlist display error"
+                });
+            }
+            return res.status(200).json({
+                success : 1,
+                data: results
+            });
+        });
+    },
+    getWishNum:(req,res) => {
+        //const body = req.body;
+        //const customer_id = decrypt(req).result.customer_id;
+        const customer_id = req.params.customer_id;
+        
+        getWishNum(customer_id, (error,results)=> {
+            if(error){
+                console.log(error);
+                return res.status(500).json({
+                    success : 0,
+                    message : "Active wishlists display error"
+                });
+            }
+            return res.status(200).json({
+                success : 1,
+                data: results
+            });
+        });
+    },
+    changeWish:(req,res) => {
+        const body = req.body;
+        //const customer_id = decrypt(req).result.customer_id;
+        
+        changeWish(body, (error,results)=> {
+            if(error){
+                console.log(error);
+                return res.status(500).json({
+                    success : 0,
+                    message : "Error in the change of items"
+                });
+            }
+            return res.status(200).json({
+                success : 1,
+                data: results
+            });
+        });
+    },
+    deleteItem:(req,res) => {
+        const body = req.body;
+        //const customer_id = decrypt(req).result.customer_id;
+        
+        deleteItem(body, (error,results)=> {
+            if(error){
+                console.log(error);
+                return res.status(500).json({
+                    success : 0,
+                    message : "Error in the change of items"
+                });
+            }
+            return res.status(200).json({
+                success : 1,
+                data: results
+            });
+        });
+    },
+    getShopping:(req,res) => {
+        const customer_id = req.params.customer_id;
+        
+        getShopping(customer_id, (error,results)=> {
+            if(error){
+                console.log(error);
+                return res.status(500).json({
+                    success : 0,
+                    message : "Error in getting the shopping cart"
+                });
+            }
+            return res.status(200).json({
+                success : 1,
+                data: results
+            });
+        });
+    },
+    getCount:(req,res) => {
+        const customer_id = req.params.customer_id;
+        
+        getCount(customer_id, (error,results)=> {
+            if(error){
+                console.log(error);
+                return res.status(500).json({
+                    success : 0,
+                    message : "Error in getting the number of wishlists"
+                });
+            }
+            return res.status(200).json({
+                success : 1,
+                data: results
+            });
+        });
     }
 }
--- api/wishlist/wishlist.service.js
+++ api/wishlist/wishlist.service.js
@@ -1,8 +1,8 @@
 const pool = require("../../config/database");
 
 module.exports = {
-    createWish: (id,data,callBack) =>{
-      pool.query(
+    createWish: (data,callBack) =>{
+      /*pool.query(
           `INSERT INTO book_list(cart_status,cart_total,type_list,cart_name,customer_id)
           SELECT "active",0,"wishlist",?,?
           FROM  dual
@@ -19,14 +19,36 @@ module.exports = {
               }
               return callBack(null, results);
           }
-      );  
+      ); */
+      pool.query(
+            `INSERT INTO book_list(cart_status,cart_total,type_list,cart_name,customer_id)
+            SELECT "active",0,"wishlist",?,?
+            FROM  dual
+            WHERE (select count(*)  FROM book_list  WHERE customer_id = ?) < 3;
+            `,
+            [
+                data.cart_name,
+                data.customer_id,
+                data.customer_id
+            ],
+            (error,results,fields) => {
+                if(error){
+                return callBack(error);
+                }
+                return callBack(null, results);
+            }
+        );
     },
     addBooksToWish: (data,callBack) =>{
         pool.query(
-            `INSERT INTO book_count(quantity,subtotal,book_isbn,cart_id) VALUES(1,0,?,?)`,
+            `INSERT INTO book_count(quantity,subtotal,book_isbn,cart_id)
+            SELECT * FROM (SELECT 1,0,?,?) as tmp
+            WHERE NOT EXISTS(SELECT book_isbn,cart_id FROM book_count WHERE book_isbn = ? AND cart_id = ?)`,
             [
                 data.book_isbn,
-                data.cart_id
+                data.cart_id,
+                data.book_isbn,
+                data.cart_id,
             ],
             (error,results,fields) => {
                 if(error){
@@ -35,5 +57,91 @@ module.exports = {
                   return callBack(null, results);
             }
         );
-    } 
+    },
+    getWish: (cart_id,callBack) =>{
+        pool.query(
+            `SELECT * FROM (SELECT books.book_title, book_count.book_count_id,book_count.cart_id FROM book_count LEFT JOIN books on books.book_isbn = book_count.book_isbn) AS res1 
+            WHERE cart_id = ?`,
+            [
+                cart_id,
+            ],
+            (error,results,fields) => {
+                if(error){
+                    return callBack(error);
+                  }
+                  return callBack(null, results);
+            }
+        );
+    },
+    getWishNum: (customer_id,callBack) =>{
+        pool.query(
+            `SELECT cart_id,cart_name FROM book_list WHERE customer_id = ? and type_list = "wishlist"`,
+            [
+                customer_id,
+            ],
+            (error,results,fields) => {
+                if(error){
+                    return callBack(error);
+                  }
+                  return callBack(null, results);
+            }
+        );
+    },
+    changeWish: (data,callBack) =>{
+        pool.query(
+            `UPDATE book_count SET cart_id = ? WHERE book_count_id = ?`,
+            [
+                data.cart_id,
+                data.book_count_id
+            ],
+            (error,results,fields) => {
+                if(error){
+                    return callBack(error);
+                  }
+                  return callBack(null, results);
+            }
+        );
+    },
+    deleteItem: (data,callBack) =>{
+        pool.query(
+            `DELETE FROM book_count WHERE book_count_id = ?`,
+            [
+                data.book_count_id
+            ],
+            (error,results,fields) => {
+                if(error){
+                    return callBack(error);
+                  }
+                  return callBack(null, results);
+            }
+        );
+    },
+    getShopping: (customer_id,callBack) =>{
+        pool.query(
+            `SELECT cart_id FROM book_list WHERE customer_id = ? AND type_list = "Shopping Cart"`,
+            [
+                customer_id
+            ],
+            (error,results,fields) => {
+                if(error){
+                    return callBack(error);
+                  }
+                  return callBack(null, results);
+            }
+        );
+    },
+    getCount: (customer_id,callBack) =>{
+        pool.query(
+            `SELECT COUNT(cart_id) AS wish_count FROM book_list where customer_id = ? and type_list = "wishlist"`,
+            [
+                customer_id
+            ],
+            (error,results,fields) => {
+                if(error){
+                    return callBack(error);
+                  }
+                  return callBack(null, results);
+            }
+        );
+    }
 };
